641f0d26eaf64bc132aba76ed612f2fb563e48de
Kendall Yee <kendallyee9893@gmail.com>
2018-11-14 16:21:57 +0800

    delete assiocation and add  http authenticate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "abc", password: "secret", 
+  except: [:index, :show]
+
   def index
     @articles =Article.all
   end
